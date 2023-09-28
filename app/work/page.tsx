e5703fdc3ad74c896d27d9fa351633bba683c880
Akshat <82810411+Afterdie@users.noreply.github.com>
2023-09-28 11:38:29 +0530

    added work page and components

---
+++ app/work/page.tsx
@@ -0,0 +1,146 @@
+import ProjectCard from "../../components/workPage/ProjectCard";
+import WorkHeader from "../../components/workPage/WorkHeader";
+import Head from "next/head";
+import { Blog } from "../../components/workPage/BlogCard";
+import { Swiper, SwiperSlide } from "swiper/react";
+import SwiperCore from "swiper";
+import Navigation from "swiper";
+import Pagination from "swiper";
+import Scrollbar from "swiper";
+import Ally from "swiper";
+import Autoplay from "swiper";
+
+import Filler from "../../components/workPage/Filler";
+import SectionHeader from "../../components/workPage/SectionHeader";
+
+let Parser = require("rss-parser");
+let parser = new Parser();
+
+SwiperCore.use([Navigation, Pagination, Scrollbar, Ally, Autoplay]);
+
+const Works = (props:any) => {
+  const { blogs } = props;
+
+  return (
+    <>
+      <Head>
+        <title>Work</title>
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+
+        <meta
+          name="description"
+          content="IoT Lab, KIIT is a team of dedicated students working under a team of competent and
+encouraging professors guiding us at every phase and step. IoT Lab is technically a research forum
+exploring the potential of crowdsourcing and Internet of Things for multidisciplinary research
+and projects with more end-user interactions."
+        ></meta>
+        <meta property="og:type" content="website"></meta>
+        <meta property="og:url" content="https://iotkiit.in"></meta>
+        <meta property="og:title" content="IoT Lab KiiT"></meta>
+        <meta
+          property="og:description"
+          content="IoT Lab, KIIT is a team of dedicated students working under a team of competent and
+encouraging professors guiding us at every phase and step. IoT Lab is technically a research forum
+exploring the potential of crowdsourcing and Internet of Things for multidisciplinary research
+and projects with more end-user interactions."
+        ></meta>
+        <meta property="og:image" content="/images/logo_small.webp"></meta>
+
+        <meta property="twitter:card" content="summary_large_image"></meta>
+        <meta property="twitter:url" content="https://iotkiit.in"></meta>
+        <meta property="twitter:title" content="IoT Lab KiiT"></meta>
+        <meta
+          property="twitter:description"
+          content="IoT Lab, KIIT is a team of dedicated students working under a team of competent and
+encouraging professors guiding us at every phase and step. IoT Lab is technically a research forum
+exploring the potential of crowdsourcing and Internet of Things for multidisciplinary research
+and projects with more end-user interactions."
+        ></meta>
+        <meta property="twitter:image" content="/images/logo_small.webp"></meta>
+      </Head>
+      <main>
+        <WorkHeader numProjects={props.numProjects} numBlogs={props.numBlogs} />
+        <SectionHeader />
+        <Filler />
+        <Swiper
+          slidesPerView={1}
+          autoplay={{ delay: 2500 }}
+          pagination={{ clickable: true }}
+          breakpoints={{
+            640: {
+              width: 640,
+              slidesPerView: 1,
+              spaceBetween: 10,
+            },
+            768: {
+              width: 768,
+              slidesPerView: 2,
+              spaceBetween: 30,
+            },
+            1200: {
+              width: 1200,
+              slidesPerView: 3,
+              spaceBetween: 10,
+            },
+          }}
+        >
+          {props.projects.map((project:any) => (
+            <SwiperSlide key={`slide-id-${project.id}`}>
+              <ProjectCard key={`project-id-${project.id}`} project={project} />
+            </SwiperSlide>
+          ))}
+        </Swiper>
+
+        {/*<Blog main={blogs.main} top={blogs.top} bottom={blogs.bottom} />*/}
+      </main>
+    </>
+  );
+};
+
+export async function getStaticProps(context:any) {
+  const SERVER = "https://api.iotkiit.in";
+
+  //Getting Projects from Server
+  const response = await fetch(`${SERVER}/items/projects`);
+  const projectsRes = await response.json();
+  const projectsData = projectsRes.data;
+  projectsData.forEach((v:any, i:any, arr:any[]) => (arr[i].imgUrl = SERVER + '/assets/' + v.imgUrl));
+
+  //Getting Feed from Medium
+  let feed = await parser.parseURL("https://medium.com/feed/iot-lab-kiit");
+  const articles_items = feed.items;
+
+  articles_items.forEach((v:any, i:any, arr:any[]) => {
+    //Converting date format
+    arr[i].date = v.pubDate.substr(0, 17);
+
+    //Assigning author to each post
+    arr[i].author = v.creator;
+
+    //Extracting thumbnail from HTML
+    arr[i].authorPic = "/images/logo_small.webp";
+
+    //Extract the first <p> tag
+    arr[i].desc =
+      v["content:encoded"].match("<p>([^<].+?)</p>")[1].substr(0, 150) + "...";
+  });
+
+  //Taking the first 3 articles
+  const [main, top, bottom] = articles_items;
+
+  return {
+    props: {
+      projects: projectsData,
+      blogs: {
+        main,
+        top,
+        bottom,
+      },
+      numProjects: projectsData?.length,
+      numBlogs: articles_items?.length,
+    },
+    revalidate: 600,
+  };
+}
+
+export default Works;
---
+++ components/workPage/Filler.tsx
@@ -0,0 +1,86 @@
+const Filler = () => {
+    return (
+      <div>
+        <section className="w-full bg-white pt-7 pb-7 md:pt-20 md:pb-24">
+          <div className="box-border flex flex-col items-center content-center px-8 mx-auto leading-6 text-black border-0 border-gray-300 border-solid md:flex-row max-w-7xl lg:px-16">
+            <div className="box-border relative w-full max-w-md px-4 mt-5 mb-4 -ml-5 text-center bg-no-repeat bg-contain border-solid md:ml-0 md:mt-0 md:max-w-none lg:mb-0 md:w-1/2 xl:pl-10">
+              <img
+                src="/images/agreement.svg"
+                alt="covid-19 help"
+                className="p-2 pl-6 pr-5 xl:pl-16 xl:pr-20 "
+              />
+            </div>
+            <div className="box-border order-first w-full text-black border-solid md:w-1/2 md:pl-10 md:order-none">
+              <h2 className="m-0 text-xl font-semibold leading-tight border-0 border-gray-300 lg:text-3xl md:text-2xl">
+                Business Boost V1.0
+              </h2>
+              <p className="pt-4 pb-6 m-0 leading-7 text-gray-700 border-0 border-gray-300 sm:pr-12 xl:pr-32 lg:text-lg">
+                We aimed for providing business, that were hit hard by the
+                pandemic, an online platform to showcase themselves
+              </p>
+              <ul className="p-0 m-0 leading-6 border-0 border-gray-300">
+                <li className="box-border relative py-1 pl-0 text-left text-gray-500 border-solid">
+                  <span className="inline-flex items-center justify-center w-6 h-6 mr-2 text-white bg-primary-default rounded-full">
+                    <span className="text-sm font-bold">✓</span>
+                  </span>{" "}
+                  Portfolio Websites
+                </li>
+                <li className="box-border relative py-1 pl-0 text-left text-gray-500 border-solid">
+                  <span className="inline-flex items-center justify-center w-6 h-6 mr-2 text-white bg-primary-default rounded-full">
+                    <span className="text-sm font-bold">✓</span>
+                  </span>{" "}
+                  Inventory and Kitchen Management Apps
+                </li>
+                <li className="box-border relative py-1 pl-0 text-left text-gray-500 border-solid">
+                  <span className="inline-flex items-center justify-center w-6 h-6 mr-2 text-white bg-primary-default rounded-full">
+                    <span className="text-sm font-bold">✓</span>
+                  </span>{" "}
+                  No Development Charges
+                </li>
+              </ul>
+            </div>
+          </div>
+          <div className="box-border flex flex-col items-center content-center px-8 mx-auto mt-2 leading-6 text-black border-0 border-gray-300 border-solid md:mt-20 xl:mt-0 md:flex-row max-w-7xl lg:px-16">
+            <div className="box-border w-full text-black border-solid md:w-1/2 md:pl-6 xl:pl-32">
+              <h2 className="m-0 text-xl font-semibold leading-tight border-0 border-gray-300 lg:text-3xl md:text-2xl">
+                Research Work
+              </h2>
+              <p className="pt-4 pb-6 m-0 leading-7 text-gray-700 border-0 border-gray-300 sm:pr-10 lg:text-lg">
+                We explore the potential of crowdsourcing and Internet of Things
+                for multidisciplinary research, aiming for
+              </p>
+              <ul className="p-0 m-0 leading-6 border-0 border-gray-300">
+                <li className="box-border relative py-1 pl-0 text-left text-gray-500 border-solid">
+                  <span className="inline-flex items-center justify-center w-6 h-6 mr-2 text-white bg-primary-default rounded-full">
+                    <span className="text-sm font-bold">✓</span>
+                  </span>{" "}
+                  Improvement in Quality of Life
+                </li>
+                <li className="box-border relative py-1 pl-0 text-left text-gray-500 border-solid">
+                  <span className="inline-flex items-center justify-center w-6 h-6 mr-2 text-white bg-primary-default rounded-full">
+                    <span className="text-sm font-bold">✓</span>
+                  </span>{" "}
+                  Helping differently-abled people
+                </li>
+                <li className="box-border relative py-1 pl-0 text-left text-gray-500 border-solid">
+                  <span className="inline-flex items-center justify-center w-6 h-6 mr-2 text-white bg-primary-default rounded-full">
+                    <span className="text-sm font-bold">✓</span>
+                  </span>{" "}
+                  Solving Real-World Problems
+                </li>
+              </ul>
+            </div>
+            <div className="box-border relative w-full max-w-md px-4 mt-10 mb-4 text-center bg-no-repeat bg-contain border-solid md:mt-0 md:max-w-none lg:mb-0 md:w-1/2">
+              <img
+                src="/images/research.svg"
+                alt="research points"
+                className="pl-4 sm:pr-10 xl:pl-10 lg:pr-32"
+              />
+            </div>
+          </div>
+        </section>
+      </div>
+    );
+  };
+  
+  export default Filler;
